fd52c55f8d6254849686849f15edfaaa9f8da08a
Lauren Birts <birtslauren@gmail.com>
2020-03-30 20:26:47 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,11 +1,11 @@
 def find_max_value(array)
   count = 0
-  max_value = -1
+  min_value = 100
   while count < array.length do
-    if max_value > array[count]
-      max_value = array[count]
+    if min_value > array[count]
+      min_value = array[count]
     end
     count += 1
   end
-  max_value
+  min_value
 end
